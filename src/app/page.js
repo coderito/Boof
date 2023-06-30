f0a3e9c9e84ff7045f37c26e38375e569e85f09f
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-06-29 23:55:18 -0400

    ocupando componentes

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
+import LadoDerecho from "./components/LadoDerecho";
+import LadoIzquiero from "./components/LadoIzquierdo";
+
 const Home = () => {
   return (
     <div className="h-screen w-screen flex flex-col md:flex-row">
     <div className="h-1/2 md:h-full w-full md:w-1/2 bg-gray-200 flex items-center justify-center">
-      <h1 className="text-4xl">Lado Izquierdo</h1>
+      <LadoIzquiero />
     </div>
     <div className="h-1/2 md:h-full w-full md:w-7/12 bg-gray-300 flex items-center justify-center">
-      <h1 className="text-4xl">Lado Derecho</h1>
+      <LadoDerecho />
     </div>
   </div>
   );
