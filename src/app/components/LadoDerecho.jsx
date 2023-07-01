f3ec33cdce69afd4a3c028bb355005e1c9f8cfc9
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-07-01 10:52:48 -0400

    iniciando markdown

--- src/app/components/LadoDerecho.jsx
+++ src/app/components/LadoDerecho.jsx
@@ -1,7 +1,9 @@
+import Hello from "../content/hello.mdx"
+
 export default function LadoDerecho() {
     return (
         <div className="h-1/2 md:h-full w-full md:w-7/12 bg-gray-300 flex items-center justify-center">
-            <h1>LadoDerecho</h1>
+            <Hello />
         </div>
     )
 }
