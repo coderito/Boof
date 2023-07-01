211724a473a946b230a39392fd60f34a824d0d0d
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-06-30 22:22:39 -0400

    creando componente lado derecho

--- src/app/components/LadoDerecho.jsx
+++ src/app/components/LadoDerecho.jsx
@@ -1,5 +1,7 @@
 export default function LadoDerecho() {
     return (
-        <h1>LadoDerecho</h1>
+        <div className="h-1/2 md:h-full w-full md:w-7/12 bg-gray-300 flex items-center justify-center">
+            <h1>LadoDerecho</h1>
+        </div>
     )
 }
