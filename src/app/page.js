df90533f1b9399371a64226d6a378669f17baa3b
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-06-30 22:22:59 -0400

    importando componentes

--- src/app/page.js
+++ src/app/page.js
@@ -4,13 +4,10 @@ import LadoIzquiero from "./components/LadoIzquierdo";
 const Home = () => {
   return (
     <div className="h-screen w-screen flex flex-col md:flex-row">
-    <div className="h-1/2 md:h-full w-full md:w-1/2 bg-gray-200 flex items-center justify-center">
       <LadoIzquiero />
-    </div>
-    <div className="h-1/2 md:h-full w-full md:w-7/12 bg-gray-300 flex items-center justify-center">
+
       <LadoDerecho />
     </div>
-  </div>
   );
 };
 
