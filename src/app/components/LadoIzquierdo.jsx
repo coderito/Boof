b96e68be2608d7da702336507e645fff2a73db47
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-07-01 15:07:56 -0400

    cambiando paddings para celulares

--- src/app/components/LadoIzquierdo.jsx
+++ src/app/components/LadoIzquierdo.jsx
@@ -7,7 +7,7 @@ import { HiMail } from "react-icons/hi";
 
 export default function LadoIzquiero() {
   return (
-    <section className="h-full w-full xl:pr-56 p-10 xl:p-24 md:w-1/2 flex items-center justify-center bg-gray-950">
+    <section className="h-full w-full xl:pr-56 p-6 xl:p-24 md:w-1/2 flex items-center justify-center bg-gray-950">
       <div className="flex flex-col gap-7">
         <h1 className="font-display text-4xl leading-[2.8rem] font-bold text-white">
           Los mejores libros 📚 que debes leer{" "}
