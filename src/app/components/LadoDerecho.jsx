c796cb743c9e4ea87680ad44f55e6b694c8a86a1
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-07-01 11:59:00 -0400

    cambiando en background a white

--- src/app/components/LadoDerecho.jsx
+++ src/app/components/LadoDerecho.jsx
@@ -1,18 +1,20 @@
-"use client"
+"use client";
 
-import Hello from "../content/hello.mdx"
+import Hello from "../content/hello.mdx";
 import { H3 } from "../components-mdx/mdxComponents";
-import { MDXProvider } from '@mdx-js/react';
+import { MDXProvider } from "@mdx-js/react";
 
 const components = {
-    h3: H3,
-  };
-
+  h3: H3,
+};
 
 export default function LadoDerecho() {
-    return (
-        <div className="h-1/2 md:h-full w-full md:w-7/12 bg-gray-300 flex items-center justify-center">
-            <MDXProvider components={components}> <Hello /></MDXProvider>
-        </div>
-    )
+  return (
+    <div className="h-1/2 md:h-full w-full md:w-7/12 bg-white flex items-center justify-center">
+      <MDXProvider components={components}>
+        {" "}
+        <Hello />
+      </MDXProvider>
+    </div>
+  );
 }
