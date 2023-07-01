adf9a8a4ff606bac0fdc36280916049a73a3ca37
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-06-30 22:22:26 -0400

    agregando iconos con hover

--- src/app/components/LadoIzquierdo.jsx
+++ src/app/components/LadoIzquierdo.jsx
@@ -1,5 +1,58 @@
+import {
+  AiFillGithub,
+  AiFillLinkedin,
+  AiFillTwitterCircle,
+} from "react-icons/ai";
+import { HiMail } from "react-icons/hi";
+
 export default function LadoIzquiero() {
-    return (
-        <h1>LadoIzquierdo</h1>
-    )
+  return (
+    <section className="h-1/2 md:h-full w-full md:pr-56 p-24 md:w-1/2 flex items-center justify-center bg-gray-950">
+      <div className="flex flex-col gap-5">
+        <h1 className="font-display text-4xl/tight font-bold text-white">
+          Los mejores libros 📚 que debes leer{" "}
+          <span className="text-yellow-300"> 1 vez en tu vida</span>
+        </h1>
+        <p className="text-sm/tight font-medium   text-gray-300">
+          Encuentra los mejores 10 libros que debes leer para tu crecimineto
+          personal
+        </p>
+        <div class="flex flex-wrap justify-center gap-x-1 gap-y-3 sm:gap-x-2 lg:justify-start">
+          <a
+            class="group relative isolate flex items-center rounded-lg px-2 py-0.5 text-[0.8125rem]/6 font-medium text-white/30 transition-colors hover:text-sky-300 gap-x-3 flex-none"
+            href="https://github.com/midudev/noticias.dev"
+          >
+            <span class="absolute inset-0 -z-10 scale-75 rounded-lg bg-white/5 opacity-0 transition group-hover:scale-100 group-hover:opacity-100"></span>
+            <AiFillGithub className="h-4 w-4" />
+            <span class="self-baseline text-white font-medium">GitHub</span>
+          </a>
+          <a
+            class="group relative isolate flex items-center rounded-lg px-2 py-0.5 text-[0.8125rem]/6 font-medium text-white/30 transition-colors hover:text-sky-300 gap-x-3 flex-none"
+            href="/rss/feed.xml"
+          >
+            <span class="absolute inset-0 -z-10 scale-75 rounded-lg bg-white/5 opacity-0 transition group-hover:scale-100 group-hover:opacity-100"></span>
+            <AiFillLinkedin className="w-4 h-4" />
+            <span class="self-baseline text-white font-medium">Linkedin</span>
+          </a>
+
+          <a
+            class="group relative isolate flex items-center rounded-lg px-2 py-0.5 text-[0.8125rem]/6 font-medium text-white/30 transition-colors hover:text-sky-300 gap-x-3 flex-none"
+            href="/rss/feed.xml"
+          >
+            <span class="absolute inset-0 -z-10 scale-75 rounded-lg bg-white/5 opacity-0 transition group-hover:scale-100 group-hover:opacity-100"></span>
+            <AiFillTwitterCircle className="w-4 h-4" />
+            <span class="self-baseline text-white font-medium">Twitter</span>
+          </a>
+          <a
+            class="group relative isolate flex items-center rounded-lg px-2 py-0.5 text-[0.8125rem]/6 font-medium text-white/30 transition-colors hover:text-sky-300 gap-x-3 flex-none"
+            href="/rss/feed.xml"
+          >
+            <span class="absolute inset-0 -z-10 scale-75 rounded-lg bg-white/5 opacity-0 transition group-hover:scale-100 group-hover:opacity-100"></span>
+            <HiMail className="w-4 h-4" />
+            <span class="self-baseline text-white font-medium">Correo</span>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
