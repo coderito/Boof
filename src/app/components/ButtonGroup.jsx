50ff16744baa8ed9ce3e7acb1880236ed3431ac3
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-07-02 19:07:40 -0400

    no mostrar button left si el indice es cero

--- src/app/components/ButtonGroup.jsx
+++ src/app/components/ButtonGroup.jsx
@@ -2,21 +2,22 @@
 
 import {AiOutlineDoubleLeft,AiOutlineDoubleRight} from "react-icons/ai"
 
-export default function ButtonGroup({handleNavegacion, setIndiceElement }) {
+export default function ButtonGroup({handleNavegacion, setIndiceElement, indiceElement }) {
   return (
     <div class="inline-flex rounded-md shadow-sm" role="group">
-      <button
+      {indiceElement !== 0 &&  <button
         onClick={() => handleNavegacion(-1)}
         type="button"
         class="inline-flex items-center px-4 gap-2 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
       >
        <AiOutlineDoubleLeft/>
         Left
-      </button>
+      </button> }
+     
       <button
         onClick={() => setIndiceElement(0)}
         type="button"
-        class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+        class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-l border-r border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
       >
         Reset
       </button>
