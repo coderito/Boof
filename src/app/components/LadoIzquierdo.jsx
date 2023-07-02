7f8f5d79785fb4bf7d9fbfe3aa1f46752819ce66
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-07-02 17:16:29 -0400

    cambiar imagen arriba

--- src/app/components/LadoIzquierdo.jsx
+++ src/app/components/LadoIzquierdo.jsx
@@ -5,10 +5,18 @@ export default function LadoIzquiero() {
   return (
     <section className="h-screen w-full xl:pr-56 p-6 xl:p-24 lg:w-1/2 flex items-center justify-center bg-gray-950">
       <div className="flex flex-col gap-7">
+        <div>
+          <img
+            class="h-auto max-w-full rounded-md cursor-pointer"
+            src="/img/img.jpg"
+            alt=""
+          />
+        </div>
         <h1 className="font-display text-4xl leading-[2.8rem] font-bold text-white">
           The best books 📚 that you should read{" "}
           <span className="text-yellow-300">1 time in your life</span>
         </h1>
+
         <p className="text-sm/tight font-medium leading-[1.4rem]  text-gray-300 text-justify">
           Discover the <span className="text-gray-300 font-semibold">10 </span>
           essential books to boost your personal growth, Immerse yourself in the
@@ -17,6 +25,7 @@ export default function LadoIzquiero() {
           of the best <span className="text-yellow-300">10 books </span>📗that
           you must read to reach your full potential.
         </p>
+
         <div class="flex flex-wrap">
           <a
             class="group relative isolate flex items-center rounded-lg px-2 py-0.5 text-[0.8125rem]/6 font-medium text-white/30 transition-colors hover:text-sky-300 gap-x-3 flex-none"
@@ -42,15 +51,6 @@ export default function LadoIzquiero() {
             <HiMail className="h-5 w-5" />
             <span class="self-baseline text-white font-medium">Mail</span>
           </a>
-        </div>
-        <div>
-          <div>
-            <img
-              class="h-auto max-w-full rounded-md cursor-pointer"
-              src="/img/img.jpg"
-              alt=""
-            />
-          </div>
         </div>
       </div>
     </section>
