ed465a3e49e314dc3e7198b9aeecb13436938c44
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-07-04 22:26:42 -0400

    prueba rendimiento

--- src/app/components-mdx/mdxComponents.jsx
+++ src/app/components-mdx/mdxComponents.jsx
@@ -1,13 +1,5 @@
 import { Typewriter } from 'react-simple-typewriter'
 
 export const H2 = ({ children }) => {
-    return <h2 className="text-4xl tracking-wide font-bold"> <Typewriter
-    words={[children]}
-    cursor
-    onLoopDone
-    cursorStyle='|'
-    typeSpeed={70}
-    deleteSpeed={50}
-    delaySpeed={1000}
-  /></h2>;
+    return <h2 className="text-4xl tracking-wide font-bold"> </h2>;
   };
