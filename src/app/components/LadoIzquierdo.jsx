91fe4245658e7067cce0e075d92d89f9f9e029d3
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-07-05 21:58:22 -0400

    letra normal en el parrafo

--- src/app/components/LadoIzquierdo.jsx
+++ src/app/components/LadoIzquierdo.jsx
@@ -3,19 +3,19 @@ import { HiMail } from "react-icons/hi";
 
 export default function LadoIzquiero() {
   return (
-    <section className="h-screen left-side w-full pt-10 xl:pr-56 p-6 xl:p-24 lg:w-1/2 flex items-center justify-center bg-gray-950">
+    <section className="h-screen left-side w-full xl:pr-56 p-6 xl:p-24 lg:w-1/2 flex items-center justify-center bg-gray-950">
     <div className="flex flex-col gap-7">
       <h1 className="font-display text-4xl text-center lg:text-start leading-[2.5rem] lg:leading-[3rem] font-bold text-white">
         The best books 📚 that you should read{" "}
         <span className="text-yellow-300 cursor-pointer hover:text-yellow-400">1 time in your life</span>
       </h1>
   
-      <p className="text-sm/tight font-semibold leading-[1.4rem] text-center   text-gray-300 lg:text-justify">
+      <p className="text-sm/tight leading-[1.4rem] text-center   text-gray-300 lg:text-justify">
         Discover the 10 essential books 📗 to boost your personal growth, Immerse
         yourself in the fascinating world of reading and discover the books
         that will transform your life! On our website, we offer you a careful
         selection of the best{" "}
-        <span className="text-yellow-300">10 books </span>that you must read
+        <span className="text-yellow-300 font-semibold">10 books </span>that you must read
         to reach your full potential.
       </p>
       <div class="flex flex-wrap justify-center lg:justify-start">
