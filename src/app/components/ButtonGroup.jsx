ac59c2429a8d2b6f45d44a47e03ecad029524c1c
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-07-06 19:15:53 -0400

    margin top 10

--- src/app/components/ButtonGroup.jsx
+++ src/app/components/ButtonGroup.jsx
@@ -12,12 +12,12 @@ export default function ButtonGroup({
   indiceElement,
 }) {
   return (
-    <div class="inline-flex rounded-md shadow-sm" role="group">
+    <div class="inline-flex rounded-md shadow-sm mt-10" role="group">
       {indiceElement !== 0 && (
         <button
           onClick={() => handleNavegacion(-1)}
           type="button"
-          class="inline-flex items-center px-4 gap-2 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+          class="inline-flex items-center px-4 gap-2 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 "
         >
           <AiOutlineDoubleLeft />
           Previos
@@ -27,7 +27,7 @@ export default function ButtonGroup({
       <button
         onClick={() => setIndiceElement(0)}
         type="button"
-        class="inline-flex items-center px-4 py-2 gap-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-l border-r border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+        class="inline-flex items-center px-4 py-2 gap-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-l border-r border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 "
       >
         <AiOutlineHome />
         Home
@@ -35,7 +35,7 @@ export default function ButtonGroup({
       <button
         onClick={() => handleNavegacion(1)}
         type="button"
-        class="inline-flex items-center px-4 py-2 gap-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+        class="inline-flex items-center px-4 py-2 gap-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 "
       >
         Next
         <AiOutlineDoubleRight />
